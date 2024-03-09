65208cb7e4d43f93bcb1186938388635933a670c
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-09 11:01:01 +0900

    3D rendering fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
-import { Avatar } from '@/components/3d/Avatar'
 import CanvasProvider from '@/providers/CanvasProvider'
+import Experience from '@/components/3d/Experience'
 import Interface from '@/components/ui/Interface'
 import Menu from '@/components/ui/Menu'
-import { Office } from '@/components/3d/Office'
-import { Suspense } from 'react'
-import { View } from '@/components/canvas/View'
-import dynamic from 'next/dynamic'
-
-// const Office = dynamic(() => import('@/components/3d/Office').then((mod) => mod.Office), { ssr: false })
 
 export default function Page() {
   return (
     <>
       <CanvasProvider>
+        <Experience />
         <main className='main'>
-          <View className='absolute top-0 z-20 flex h-screen w-full flex-col items-center justify-center'>
-            <Office />
-          </View>
           <Interface />
         </main>
       </CanvasProvider>
--- src/components/3d/Experience.tsx
+++ src/components/3d/Experience.tsx
@@ -1,11 +1,16 @@
-import { Office } from './Office'
+'use client'
+
+import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
+
+const OfficeModel = dynamic(() => import('@/components/3d/OfficeModel').then((mod) => mod.default), { ssr: false })
 
 const Experience = () => {
   return (
-    <>
+    <Suspense fallback={null}>
       <ambientLight intensity={1} />
-      <Office />
-    </>
+      <OfficeModel />
+    </Suspense>
   )
 }
 export default Experience
